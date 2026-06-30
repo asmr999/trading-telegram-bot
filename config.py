9996ecf49987884945741bb271a24eeb7fb9efb3
asmr999 <asmrjr5@gmail.com>
2026-06-30 18:20:46 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# رابط الإحالة
+JUSTMARKETS_REF_LINK = "https://justmarkets.com"
+
+# كود الأمان لقراءة التوكن المخفي
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
